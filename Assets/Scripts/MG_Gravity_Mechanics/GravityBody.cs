2b071fc1293db9d8d287cf87324724dbaca129f7
LlanosNing <llanosning@gmail.com>
2024-10-21 23:53:30 +0200

    :(

--- Assets/Scripts/MG_Gravity_Mechanics/GravityBody.cs
+++ Assets/Scripts/MG_Gravity_Mechanics/GravityBody.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class GravityBody : MonoBehaviour
 {
-    private static float GRAVITY_FORCE = 800;
+    public static float GRAVITY_FORCE = 900;
 
     public Vector3 GravityDirection
     {
