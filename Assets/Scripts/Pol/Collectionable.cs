7abefa2e554e54c582510f4ac22733e3bcee62bc
Cuplexx <pol.oficial05@gmail.com>
2024-10-22 12:19:17 +0200

    awd

--- Assets/Scripts/Pol/Collectionable.cs
+++ Assets/Scripts/Pol/Collectionable.cs
@@ -5,12 +5,22 @@ using UnityEngine;
 public class Collectionable : MonoBehaviour
 {
     public GameObject coin;
+    public Animator anim;
+    public AudioSource audioRef;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            Destroy(coin);
+            anim.SetTrigger("CatchCoin");
+            audioRef.Play();
+            StartCoroutine(delayDestroy());
         }
+    }
+
+    public IEnumerator delayDestroy()
+    {
+        yield return new WaitForSeconds(1f);
+        Destroy(coin);
     }
 }
