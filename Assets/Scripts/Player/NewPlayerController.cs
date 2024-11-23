f081f8ac9492a25650b6030f8268fef113d61fcf
MarioCampelloRodes <mcampellor@esimurcia.com>
2024-11-23 12:23:15 +0100

    backup script movimiento

--- Assets/Scripts/Player/NewPlayerController.cs
+++ Assets/Scripts/Player/NewPlayerController.cs
@@ -1,187 +1,169 @@
-//using UnityEngine;
-//using UnityEngine.InputSystem;
+using UnityEngine;
 
-//public class PlayerController : MonoBehaviour
-//{
-//    public float rayCastLength;
-//    public float rotationSpeed;
-//    private float tmpRotationSpeed;
-//    public float speed;
-//    public float gravity;
-//    private float tmpGravity;
-//    public float jumpForce;
-//    private Rigidbody rb;
-//    public Transform currentPlanet;
-//    public Transform playerVisual;
+public class PlayerController : MonoBehaviour
+{
+    public float rayCastLength;
+    public float rotationSpeed;
+    private float tmpRotationSpeed;
+    public float speed;
+    public float gravity;
+    private float tmpGravity;
+    public float jumpForce;
+    private Rigidbody rb;
+    public Transform currentPlanet;
+    public Transform playerVisual;
 
-//    RaycastHit[] hits;
-//    Vector3 planetDir;
-//    Vector3 normalVector;
-//    Vector3 input;
+    RaycastHit[] hits;
+    Vector3 planetDir;
+    Vector3 normalVector;
+    Vector3 input;
 
-//    public bool isTouchingPlanetSurface = false;
-//    private Transform MainCameraTransform;
-//    public Transform CameraArmTransform;
+    public bool isTouchingPlanetSurface = false;
+    private Transform MainCameraTransform;
+    public Transform CameraArmTransform;
 
-//    PlayerControls playerControls;
-//    Animator anim;
+    bool CanJump = true;
+    bool slowDown = false;
 
-//    bool CanJump = true;
-//    bool slowDown = false;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        MainCameraTransform = Camera.main.transform;
+        tmpGravity = gravity;
+        tmpRotationSpeed = rotationSpeed;
+    }
 
-//    private void Awake()
-//    {
-//        rb = GetComponent<Rigidbody>();
-//        MainCameraTransform = Camera.main.transform;
-//        anim = GetComponent<Animator>();
-//        tmpGravity = gravity;
-//        tmpRotationSpeed = rotationSpeed;
+    private void FixedUpdate()
+    {
+        Movement();
+        ApplyGravity();
+        ApplyPlanetRotation();
+    }
 
-//        playerControls = new PlayerControls();
-//        playerControls.Movement.GamepadEastButton.performed += Jump;
-//    }
+    void Jump()
+    {
+        if (!CanJump) return;
+        rb.velocity *= 0;
+        rb.AddForce(normalVector * jumpForce, ForceMode.Impulse);
+        gravity = tmpGravity / 2f;
+        Invoke(nameof(RestoreGravity), 1f);
+        CanJump = false;
+        rotationSpeed = tmpRotationSpeed / 2f;
+    }
 
-//    private void OnEnable()
-//    {
-//        playerControls.Enable();
-//    }
+    void RestoreGravity()
+    {
+        gravity = tmpGravity;
+        CanJump = true;
+        slowDown = false;
+    }
 
-//    private void OnDisable()
-//    {
-//        playerControls.Disable();
-//    }
+    public void EnterNewGravityField()
+    {
+        gravity = tmpGravity / 4f;
+        rb.velocity *= .5f;
+        rotationSpeed = tmpRotationSpeed / 10f;
+        slowDown = true;
+        CanJump = false;
+        Invoke(nameof(RestoreGravity), .5f);
+    }
 
-//    private void FixedUpdate()
-//    {
-//        Movement();
-//        ApplyGravity();
-//        ApplyPlanetRotation();
-//    }
+    void Movement()
+    {
+        input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        Vector3 cameraRotation = new Vector3(0, MainCameraTransform.localEulerAngles.y + CameraArmTransform.localEulerAngles.y, 0);
+        Vector3 Dir = Quaternion.Euler(cameraRotation) * input;
+        Vector3 movement_dir = (transform.forward * Dir.z + transform.right * Dir.x);
+        Vector3 currentNormalVelocity = Vector3.Project(rb.velocity, normalVector.normalized);
+        rb.velocity = currentNormalVelocity + (movement_dir * speed);
 
-//    void Jump(InputAction.CallbackContext context)
-//    {
-//        if (!CanJump) return;
-//        rb.velocity *= 0;
-//        rb.AddForce(normalVector * jumpForce, ForceMode.Impulse);
-//        gravity = tmpGravity / 2f;
-//        Invoke(nameof(RestoreGravity), 1f);
-//        CanJump = false;
-//        rotationSpeed = tmpRotationSpeed / 2f;
-//    }
+        if (movement_dir != Vector3.zero)
+        {
+            //anim.SetBool("IsMoving", true);
+            playerVisual.localRotation = Quaternion.LookRotation(Dir);
+        }
+        else
+        {
+            //anim.SetBool("IsMoving", false);
+        }
+        if (slowDown)
+            rb.velocity *= .5f;
+    }
 
-//    void RestoreGravity()
-//    {
-//        gravity = tmpGravity;
-//        CanJump = true;
-//        slowDown = false;
-//    }
+    void ApplyGravity()
+    {
+        if (currentPlanet == null) return;
 
-//    public void EnterNewGravityField()
-//    {
-//        gravity = tmpGravity / 4f;
-//        rb.velocity *= .5f;
-//        rotationSpeed = tmpRotationSpeed / 10f;
-//        slowDown = true;
-//        CanJump = false;
-//        Invoke(nameof(RestoreGravity), .5f);
-//    }
+        hits = Physics.RaycastAll(transform.position, -transform.up, rayCastLength);
 
-//    void Movement()
-//    {
-//        input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
-//        Vector3 cameraRotation = new Vector3(0, MainCameraTransform.localEulerAngles.y + CameraArmTransform.localEulerAngles.y, 0);
-//        Vector3 Dir = Quaternion.Euler(cameraRotation) * input;
-//        Vector3 movement_dir = (transform.forward * Dir.z + transform.right * Dir.x);
-//        Vector3 currentNormalVelocity = Vector3.Project(rb.velocity, normalVector.normalized);
-//        rb.velocity = currentNormalVelocity + (movement_dir * speed);
+        if (hits.Length == 0)
+        {
+            hits = Physics.RaycastAll(transform.position, transform.forward, rayCastLength);
+        }
 
-//        if (movement_dir != Vector3.zero)
-//        {
-//            anim.SetBool("IsMoving", true);
-//            playerVisual.localRotation = Quaternion.LookRotation(Dir);
-//        }
-//        else
-//        {
-//            anim.SetBool("IsMoving", false);
-//        }
-//        if (slowDown)
-//            rb.velocity *= .5f;
-//    }
+        if (hits.Length == 0)
+        {
+            hits = Physics.RaycastAll(transform.position, -transform.forward, rayCastLength);
+        }
 
-//    void ApplyGravity()
-//    {
-//        if (currentPlanet == null) return;
+        if (hits.Length == 0)
+        {
+            hits = Physics.RaycastAll(transform.position, transform.right, rayCastLength);
+        }
 
-//        hits = Physics.RaycastAll(transform.position, -transform.up, rayCastLength);
+        if (hits.Length == 0)
+        {
+            hits = Physics.RaycastAll(transform.position, -transform.right, rayCastLength);
+        }
 
-//        if (hits.Length == 0)
-//        {
-//            hits = Physics.RaycastAll(transform.position, transform.forward, rayCastLength);
-//        }
+        if (hits.Length == 0)
+        {
+            planetDir = currentPlanet.position - transform.position;
+            hits = Physics.RaycastAll(transform.position, planetDir, rayCastLength);
+        }
 
-//        if (hits.Length == 0)
-//        {
-//            hits = Physics.RaycastAll(transform.position, -transform.forward, rayCastLength);
-//        }
+        GetPlanetNormal();
+        rb.AddForce(normalVector.normalized * gravity, ForceMode.Acceleration);
+        hits = new RaycastHit[0];
+    }
 
-//        if (hits.Length == 0)
-//        {
-//            hits = Physics.RaycastAll(transform.position, transform.right, rayCastLength);
-//        }
+    void ApplyPlanetRotation()
+    {
+        Quaternion targetRotation = Quaternion.FromToRotation(transform.up, normalVector) * transform.rotation;
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, rotationSpeed * Time.fixedDeltaTime);
+        if (isTouchingPlanetSurface && CanJump)
+            rotationSpeed = tmpRotationSpeed;
+    }
 
-//        if (hits.Length == 0)
-//        {
-//            hits = Physics.RaycastAll(transform.position, -transform.right, rayCastLength);
-//        }
+    void GetPlanetNormal()
+    {
+        if (currentPlanet == null) return;
+        normalVector = (transform.position - currentPlanet.position).normalized;
+        for (int i = 0; i < hits.Length; i++)
+        {
+            if (hits[i].transform == currentPlanet)
+            {
+                normalVector = hits[i].normal.normalized;
+                break;
+            }
+        }
+        return;
+    }
 
-//        if (hits.Length == 0)
-//        {
-//            planetDir = currentPlanet.position - transform.position;
-//            hits = Physics.RaycastAll(transform.position, planetDir, rayCastLength);
-//        }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform == currentPlanet)
+        {
+            isTouchingPlanetSurface = true;
+        }
+    }
 
-//        GetPlanetNormal();
-//        rb.AddForce(normalVector.normalized * gravity, ForceMode.Acceleration);
-//        hits = new RaycastHit[0];
-//    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.transform == currentPlanet)
+        {
+            isTouchingPlanetSurface = false;
+        }
+    }
 
-//    void ApplyPlanetRotation()
-//    {
-//        Quaternion targetRotation = Quaternion.FromToRotation(transform.up, normalVector) * transform.rotation;
-//        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, rotationSpeed * Time.fixedDeltaTime);
-//        if (isTouchingPlanetSurface && CanJump)
-//            rotationSpeed = tmpRotationSpeed;
-//    }
-
-//    void GetPlanetNormal()
-//    {
-//        if (currentPlanet == null) return;
-//        normalVector = (transform.position - currentPlanet.position).normalized;
-//        for (int i = 0; i < hits.Length; i++)
-//        {
-//            if (hits[i].transform == currentPlanet)
-//            {
-//                normalVector = hits[i].normal.normalized;
-//                break;
-//            }
-//        }
-//        return;
-//    }
-
-//    private void OnTriggerEnter(Collider other)
-//    {
-//        if (other.transform == currentPlanet)
-//        {
-//            isTouchingPlanetSurface = true;
-//        }
-//    }
-
-//    private void OnTriggerExit(Collider other)
-//    {
-//        if (other.transform == currentPlanet)
-//        {
-//            isTouchingPlanetSurface = false;
-//        }
-//    }
-
-//}
+}
