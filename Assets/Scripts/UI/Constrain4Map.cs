c44a35429822eae588dc7da15da28fc1f700af47
MarioCampelloRodes <mcampellor@esimurcia.com>
2025-01-25 11:41:23 +0100

    xd

--- Assets/Scripts/UI/Constrain4Map.cs
+++ Assets/Scripts/UI/Constrain4Map.cs
@@ -7,9 +7,7 @@ public class Constrain4Map : MonoBehaviour
 {
     public Transform target;
     public Transform map_obj; //Plano Minimapa para saber a qué altura está
-    public bool positionConstrain = true;
     public bool rotationContrain = true;
-    public float offsetY = 0.1f; //Para que no se solapen los elementos
 
     void Start()
     {
@@ -19,17 +17,10 @@ public class Constrain4Map : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(positionConstrain == true)
-        {
-            //transform.position = new Vector3(target.position.x, map_obj.position.y + offsetY, target.position.z);
-        }
         if (rotationContrain == true)
         {
             transform.rotation = target.rotation;
 
         }
-
-
-
     }
 }
