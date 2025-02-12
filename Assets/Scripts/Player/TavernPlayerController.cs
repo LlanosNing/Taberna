92380d7137d40097767267aaf8095284e72659be
MarioCampelloRodes <mcampellor@esimurcia.com>
2025-02-12 10:39:29 +0100

    Controlador Taverna Primitivo

--- Assets/Scripts/Player/TavernPlayerController.cs
+++ Assets/Scripts/Player/TavernPlayerController.cs
@@ -1,18 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations;
 
 public class TavernPlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float moveSpeed = 5f;
+    public float lookRotSpeed = 10f;
+    Vector2 input;
+    public Transform cameraTransform; // Arrastra aquí la cámara fija
+    Rigidbody rb;
+    public Transform playerVisual;
+
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        Move();
+    }
+
+    void Move()
+    {
+        Vector3 direction = new Vector3(input.x, 0f, input.y).normalized;
+
+        // Convertir la dirección de entrada a espacio relativo a la cámara
+        Vector3 moveDir = cameraTransform.forward * direction.z + cameraTransform.right * direction.x;
+        moveDir.y = 0f; // Evitar que el personaje se incline
+
+        rb.velocity = moveDir.normalized * moveSpeed;
+
+        Quaternion lookDirection = Quaternion.LookRotation(moveDir);
+        if(moveDir !=  Vector3.zero)
+        {
+            playerVisual.localRotation = Quaternion.Slerp(playerVisual.localRotation, lookDirection, Time.deltaTime * lookRotSpeed);
+        }
+
+        if(input == Vector2.zero)
+        {
+            rb.velocity = Vector3.zero;
+        }
     }
 }
