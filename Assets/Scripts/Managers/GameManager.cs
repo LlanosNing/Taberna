8d62660a52b6ebdf962f7018c41fb1a2cefaba70
MarioCampelloRodes <mcampellor@esimurcia.com>
2025-03-14 13:26:10 +0100

    Preparando SaveFiles y Ordenando Jerarquía

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -6,124 +6,24 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     private FadeScreen _fd;
-    //public float waitToRespawn;
-    //public float enemyRespawn;
-    //public float pickupRespawn;
 
     public string levelToLoad;
 
+    public static bool hasPortalTwoKey;
+    public static bool hasPortalThreeKey;
+    public static int lastPortal;
+    public static int questNumber;
+    public static int joaquinDialog;
 
-    //public int gemCollected;
-
-    //private PlayerController _pCReference;
-    //private CheckpointController _cReference;
-    //private UIController _uIReference;
-    //private PlayerHealthController _pHReference;
-    //private SlimePlayer _sPReference;
-
-    //public GameObject[] horizontalEnemies;
-    //public GameObject[] verticalEnemies;
-    //public GameObject[] chasingEnemies;
-    //public GameObject[] pickups;
-
-    private void Awake()
-    {
-        _fd = GameObject.Find("FSmanager").GetComponent<FadeScreen>();
-    }
     void Start()
     {
-        _fd.enabled = true;
-        //_pCReference = GameObject.Find("Player").GetComponent<PlayerController>();
-        //_cReference = GameObject.Find("CheckpointController").GetComponent<CheckpointController>();
-        //_uIReference = GameObject.Find("Canvas").GetComponent<UIController>();
-        //_pHReference = GameObject.Find("Player").GetComponent<PlayerHealthController>();
-        //_sPReference = GameObject.Find("Player").GetComponent<SlimePlayer>();
+
     }
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.P))
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
-
-    //public void RespawnPlayer()
-    //{
-    //    StartCoroutine(RespawnPlayerCo());
-    //}
-
-    //public void RespawnEnemy()
-    //{
-    //    StartCoroutine(RespawnHorizontalEnemyCo());
-    //    StartCoroutine(RespawnVerticalEnemyCo());
-    //    StartCoroutine(RespawnChasingEnemyCo());
-    //}
-    //public void RespawnPickups()
-    //{
-    //    StartCoroutine(RespawnPickupsCo());
-    //}
-
-    //private IEnumerator RespawnPlayerCo()
-    //{
-    //    _pCReference.gameObject.SetActive(false);
-    //    _sPReference.NormalStats();
-    //    yield return new WaitForSeconds(waitToRespawn);
-    //    _pCReference.gameObject.SetActive(true);
-    //    _pCReference.transform.position = _cReference.spawnPoint;
-    //    _pHReference.currentHealth = _pHReference.maxHealth;
-    //    _uIReference.UpdateHealthDisplay();
-    //}
-
-    //private IEnumerator RespawnHorizontalEnemyCo()
-    //{
-    //    for (int i = 0; i < horizontalEnemies.Length; i++) //poner el nombre del array.Length
-    //    {
-    //        horizontalEnemies[i].SetActive(false);//con la i se recorre todos los elementos del array
-    //    }
-    //    for (int i = 0; i < horizontalEnemies.Length; i++)
-    //    {
-    //        horizontalEnemies[i].SetActive(true);
-    //    }
-
-    //    yield return new WaitForSeconds(enemyRespawn);
-    //}
-    //private IEnumerator RespawnVerticalEnemyCo()
-    //{
-    //    for (int i = 0; i < verticalEnemies.Length; i++)
-    //    {
-    //        verticalEnemies[i].SetActive(false);
-    //    }
-    //    for (int i = 0; i < verticalEnemies.Length; i++)
-    //    {
-    //        verticalEnemies[i].SetActive(true);
-    //    }
-
-    //    yield return new WaitForSeconds(enemyRespawn);
-    //}
-    //private IEnumerator RespawnChasingEnemyCo()
-    //{
-    //    for (int i = 0; i < chasingEnemies.Length; i++)
-    //    {
-    //        chasingEnemies[i].SetActive(false);
-    //    }
-    //    for (int i = 0; i < verticalEnemies.Length; i++)
-    //    {
-    //        chasingEnemies[i].SetActive(true);
-    //    }
-
-    //    yield return new WaitForSeconds(enemyRespawn);
-    //}
-    //private IEnumerator RespawnPickupsCo()
-    //{
-    //    for (int i = 0; i < pickups.Length; i++)
-    //    {
-    //        pickups[i].SetActive(false);
-    //    }
-    //    for (int i = 0; i < pickups.Length; i++)
-    //    {
-    //        pickups[i].SetActive(true);
-    //    }
-
-    //    yield return new WaitForSeconds(pickupRespawn);
-    //}
 
     public void ExitLevel()
     {
