b6121c80fe9c125204728e74e26aef7e777e09d0
MarioCampelloRodes <mcampellor@esimurcia.com>
2025-03-24 11:02:17 +0100

    Preparando animación topo 2

--- Assets/Scripts/NPCs/Mole.cs
+++ Assets/Scripts/NPCs/Mole.cs
@@ -8,9 +8,9 @@ public class Mole : MonoBehaviour
 
     public float minTimeToAppear, maxTimeToAppear;
     float timeToAppearCounter;
-    public bool hasToAppear;
     public float maxTimeToHide;
     float timeToHideCounter;
+
 
     private void Start()
     {
@@ -20,31 +20,19 @@ public class Mole : MonoBehaviour
 
     private void Update()
     {
-        if (animRef.GetCurrentAnimatorStateInfo(0).IsName("Underground") && !hasToAppear && timeToAppearCounter <= 0)
+        if (animRef.GetCurrentAnimatorStateInfo(0).IsName("Underground") && timeToAppearCounter <= 0)
         {
             timeToAppearCounter = Random.Range(minTimeToAppear, maxTimeToAppear);
             Debug.Log("El topo " + gameObject.name + " tardará " +  timeToAppearCounter + " segundos en aparecer");
-            hasToAppear = true;
         }
 
         if(timeToAppearCounter > 0)
         {
             timeToAppearCounter -= Time.deltaTime;
         }
-        if(timeToAppearCounter <= 0 && hasToAppear)
+        if(timeToAppearCounter <= 0)
         {
             animRef.SetTrigger("Jump");
-        }
-
-        if (animRef.GetCurrentAnimatorStateInfo(0).IsName("Underground"))
-        {
-            timeToHideCounter -= Time.deltaTime;
-
-            if(timeToHideCounter <= 0)
-            {
-                timeToHideCounter = maxTimeToHide;
-                hasToAppear = false;
-            }
         }
     }
 }
